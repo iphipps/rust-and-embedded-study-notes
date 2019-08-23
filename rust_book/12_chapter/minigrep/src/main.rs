f2a7171164bcd3bffaf56a6b030910f8140fb97c
Ian Phipps <phippsian@gmail.com>
2019-08-23 18:13:14 +0000

    Handle error returns

--- rust_book/12_chapter/minigrep/src/main.rs
+++ rust_book/12_chapter/minigrep/src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::error::Error;
 use std::fs;
 use std::process;
 
@@ -11,7 +12,12 @@ fn main() {
     });
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
-    run(config);
+
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+
+        process::exit(1);
+    }
 }
 
 struct Config {
@@ -31,10 +37,10 @@ impl Config {
         Ok(Config { query, filename })
     }
 }
-
-fn run(config: Config) {
-    let contents =
-        fs::read_to_string(config.filename).expect("Something went wrong reading the file");
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
 
     println!("With text:\n{}", contents);
+
+    Ok(())
 }
--- rust_book/12_chapter/nodes.md
+++ rust_book/12_chapter/nodes.md
@@ -6,3 +6,6 @@ ripgrep
 
 
 `cargo run the poem.txt
+
+To see the refactor steps on the (file)[./minigrep/src/main.rs] check the
+(git history)[https://github.com/iphipps/rust-study-notes/commits/master/rust_book/12_chapter/minigrep/src/main.rs]
